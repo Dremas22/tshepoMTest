b2358041e02fb167da6fabb01a3cb3a8ee017a42
Dremas22 <76808634+Dremas22@users.noreply.github.com>
2024-10-28 23:33:25 +0200

    Setting up nextjs environment

---
+++ bicitytshepotest-app/components/clients.js
@@ -0,0 +1,9 @@
+function MyClients () {
+    return (
+        <div>
+          <h1>BinaryCity assessment</h1>
+        </div>
+      );
+}
+
+export default MyClients;
---
+++ bicitytshepotest-app/pages/index.js
@@ -0,0 +1,8 @@
+import MyClients from "@/components/clients";
+export default function Home() {
+  return (
+    <div>
+      <MyClients />
+    </div>
+  );
+}
